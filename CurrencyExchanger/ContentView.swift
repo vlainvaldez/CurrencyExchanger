8ab396471678b131fad7603b3428400c340f9084
alvin joseph valdez <alvinjosephvaldez@alvins-MacBook-Pro.local>
2019-12-05 22:43:51 +0800

    initial commit

---
+++ CurrencyExchanger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyExchanger
+//
+//  Created by alvin joseph valdez on 12/5/19.
+//  Copyright © 2019 alvin joseph valdez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
